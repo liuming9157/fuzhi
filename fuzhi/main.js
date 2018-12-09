44406a6124d01fa2b61b66e55f8ededa658d3bc2
Jack Liu <liuming@mzsat.cn>
2018-12-09 16:46:20 +0800

    first time

---
+++ fuzhi/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+Vue.prototype.$api='https://www.yaochele.com/api/fuzhi'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
